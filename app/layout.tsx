6c235d3da143389e309c92a9b95f32cd715a297e
chan9yu <dev.cgyeo@gmail.com>
2025-02-20 00:18:30 +0900

    feat: kakao map 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 
 import localFont from 'next/font/local'
+import Script from 'next/script'
 
 import QueryProvider from '@/shared/query/provider'
 
@@ -20,10 +21,14 @@ export default function RootLayout({
     <html lang="ko" className={pretendard.variable}>
       <body className="font-sans">
         <QueryProvider>
-          <div className="relative mx-auto h-dvh w-full max-w-[400px] bg-background px-4 sm:w-[400px]">
+          <div className="bg-background relative mx-auto h-dvh w-full max-w-[400px] px-4 sm:w-[400px]">
             {children}
           </div>
         </QueryProvider>
+        <Script
+          src="//dapi.kakao.com/v2/maps/sdk.js?appkey=8773e5bd8b2289d110206a4c37365a40&libraries=services,clusterer&autoload=false"
+          strategy="beforeInteractive"
+        />
       </body>
     </html>
   )
